60529db76eb503b7f0498a238cb9b1066ddd9a49
JP Lokuku <hnryjsph@outlook.com>
2022-10-19 22:51:52 +0300

    Adding my projects

---
+++ printing.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char testGrade = 'A';
+    char name[] = "Henry Joseph";
+    printf("%s, your grade is %c", name, testGrade);
+
+    /*
+    %c -  character
+    %d - integer number (base10)
+    %e - exponetial floating-point number
+    %f - floating point number
+    %i - integer (base 10)
+    %o - octal number
+    %s - a string of characters
+    %u - unsigned decimal (integer) number
+    %x - number in hexadecimal (base 16)
+    %% - print percentage sign
+    \% - print a percetage sign
+    */
+}
---
+++ variables.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // char must take single quotes
+    char testGrade = 'A'; // 8 bit character
+    char name[] = "Mike";
+
+    short age0 = 10;
+    int age1 = 20;
+    long age2 = 30;
+    long long age3 = 40;
+
+    float gpa0 = 2.5;
+    double gpa1 = 3.5;
+    long double gpa2 = 3.5;
+
+    int isTall;
+    isTall = 1;
+    
+    const int ISTALL;
+
+    /*
+    A signed integer is on that has both negative and positive digits while an unsigned
+    number has only postive
+    
+    */
+
+}
